f627dfdfa65d83c593e40e11a0900d6e81067475
이창곤 <rhsdl970110@naver.com>
2024-03-18 11:48:02 +0900

    💠 feat: 자동 로그인 상태 관리 로직 추가

---
+++ frontend/lib/user/screens/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/providers/login_provider.dart';
+import 'package:provider/provider.dart';
+import 'login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    checkLoginStatus();
+  }
+
+  void checkLoginStatus() async {
+    final loginProvider = Provider.of<LoginProvider>(context, listen: false);
+    await loginProvider.checkToken();
+    if (loginProvider.isLoggedIn) {
+      //기존 토큰이 확인 되었을 때 로직
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+    } else {
+      //기존 토큰이 없을 시 로그인 화면으로 이동 로직
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        // child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
