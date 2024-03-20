394c65f9039d9c9903763114ed084483d968e8bd
최용수 <cys980220@gmail.com>
2024-03-20 16:39:08 +0900

    fix: 카드 크롤링 카드사 별 크롤링(S10P22D110-86)

--- backend/src/main/java/com/ssafy/pickachu/scheduler/ActionCrawlingScheduler.java
+++ backend/src/main/java/com/ssafy/pickachu/scheduler/ActionCrawlingScheduler.java
@@ -3,15 +3,29 @@ package com.ssafy.pickachu.scheduler;
 import com.ssafy.pickachu.crawling.service.CrawlingService;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ActionCrawlingScheduler {
 
     private static CrawlingService crawlingService;
 
-
-
-    @Scheduled(fixedRate = 1000*60*60*24) // 하루 주기 크롤링
+    private int cardNum = 0;
+    private static Map<Integer, String> CARD_COMPANY_CODE = new HashMap<>(){{
+       put(0, "32");
+       put(1, "1");
+       put(2, "2");
+       put(3, "3");
+       put(4, "4");
+       put(5, "5");
+       put(6, "7");
+       put(7, "8");
+       put(8, "9");
+       put(9, "10");
+    }};
+    @Scheduled(fixedRate = 1000*60*60*24) // 하루 주기 카드사 하나 씩 크롤링
     public void auctionClosing() {
-        crawlingService.CrawlingCards();
+        crawlingService.CrawlingCards(CARD_COMPANY_CODE.get(cardNum++));
     }
 
 
