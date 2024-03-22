a04b2323c324885d1eda76f013a956ebecbdc685
금예인 <tbc07061@naver.com>
2024-03-22 10:27:51 +0900

    Revert "Merge branch 'be/dev' into 'master'"
    This reverts commit 7484a495dbf61b4dc3ff47d19529d2aa48b241c0, reversing
    changes made to b0946803f729c5802b1c5070e112ad47cb7f4bf8.

--- backend/src/main/java/com/ssafy/pickachu/cards/repository/CardInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.pickachu.cards.repository;
-
-
-import com.ssafy.pickachu.cards.entity.CardInfo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CardInfoRepository extends MongoRepository<CardInfo, String> {
-}
--- backend/src/main/java/com/ssafy/pickachu/crawling/service/CrawlingService.java
+++
@@ -1,6 +0,0 @@
-package com.ssafy.pickachu.crawling.service;
-
-public interface CrawlingService {
-
-    public void CrawlingCards(String cardCompanyId);
-}
