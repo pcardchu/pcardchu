eca0db53ff769ec0b75855360c2bb5d96b05796c
정수진 <dmg06017@naver.com>
2024-03-27 16:51:29 +0900

    🛠 fix: api 경로 제거

--- backend/src/main/java/com/ssafy/pickachu/domain/statistics/controller/StatisticsController.java
+++ backend/src/main/java/com/ssafy/pickachu/domain/statistics/controller/StatisticsController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin("*")
-@RequestMapping("/api/statistics")
+@RequestMapping("/statistics")
 public class StatisticsController {
 
     private final StatisticsService statisticsService;
