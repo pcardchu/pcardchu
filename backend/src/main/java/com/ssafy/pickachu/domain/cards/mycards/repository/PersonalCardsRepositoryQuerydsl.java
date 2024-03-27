54f25a2fa708d937cd32a928f9d328409371a7e4
정수진 <dmg06017@naver.com>
2024-03-27 17:50:27 +0900

    🛠 fix: 파일 중복 버그 수정

--- backend/src/main/java/com/ssafy/pickachu/domain/cards/mycards/repository/PersonalCardsRepositoryQuerydsl.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.pickachu.domain.cards.mycards.repository;
-
-import java.util.List;
-
-public interface PersonalCardsRepositoryQuerydsl {
-    List<String> getPersonalCardsRankingList();
-}
