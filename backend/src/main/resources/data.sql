3809a40f8e32cf1567d2b87b5bfbe6d53b2b0fac
금예인 <tbc07061@naver.com>
2024-03-28 12:10:37 +0900

    🛠 fix: data.sql 수정

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO user(nickname, provider, identifier, role, email, createTime)
+INSERT INTO user(nickname, provider, identifier, role, email, create_time)
 VALUES ("test", "pickachu", "pickachu_test", "ROLE_USER", "test@pickachu.com", NOW());
