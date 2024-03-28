79f7ff8fe6ffdf0b2d8167da22d4ef8224b2a0f8
박설연 <s303y@naver.com>
2024-03-28 13:44:46 +0900

    💭 chore: home UI 깨짐 수정

--- frontend/lib/home/widgets/ToListCard.dart
+++ frontend/lib/home/widgets/ToListCard.dart
@@ -16,73 +16,70 @@ class ToListCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(12.0),
         ),
         color: Color(0xFFFFF7C5),
-        child: Center(
-          child: Container(
-            width: ScreenUtil.w(83),
-            height: ScreenUtil.h(16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      child: Text(
-                        '고객님! 새 카드를 찾으시나요?',
-                        style: AppFonts.suit(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w800,
-                            color: AppColors.textBlack),
+        child: Container(
+          margin: EdgeInsets.only(left: 10, right: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Text(
+                      '고객님! 새 카드를 찾으시나요?',
+                      style: AppFonts.suit(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w800,
+                          color: AppColors.textBlack),
+                    ),
+                  ),
+                  SizedBox(
+                    height: ScreenUtil.h(1),
+                  ),
+                  Container(
+                    width: ScreenUtil.w(53),
+                    height: ScreenUtil.h(4),
+                    margin: EdgeInsets.only(left: 10),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => CardList()));
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.mainWhite,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: ScreenUtil.h(1),
-                    ),
-                    Container(
-                      width: ScreenUtil.w(53),
-                      height: ScreenUtil.h(4),
-                      margin: EdgeInsets.only(left: 10),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => CardList()));
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: AppColors.mainWhite,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '나에게 맞는 혜택별 알짜카드', // 텍스트
+                            style: AppFonts.suit(
+                              color: AppColors.subLightBlue,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w800,
+                            ), // 텍스트 색상
                           ),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              '나에게 맞는 혜택별 알짜카드', // 텍스트
-                              style: AppFonts.suit(
-                                color: AppColors.subLightBlue,
-                                fontSize: 12,
-                                fontWeight: FontWeight.w800,
-                              ), // 텍스트 색상
-                            ),
-                            Icon(
-                              Icons.keyboard_arrow_right_rounded, // 아이콘
-                              color: AppColors.mainBlue, // 아이콘 색상
-                            ),
-                          ],
-                        ),
+                          Icon(
+                            Icons.keyboard_arrow_right_rounded, // 아이콘
+                            color: AppColors.mainBlue, // 아이콘 색상
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
-                Image.asset(
-                  'assets/images/heart_icon.png',
-                  width: ScreenUtil.w(20),
-                  height: ScreenUtil.w(20),
-                  fit: BoxFit.contain,
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+              Image.asset(
+                'assets/images/heart_icon.png',
+                width: ScreenUtil.w(16),
+                height: ScreenUtil.w(16),
+                fit: BoxFit.contain,
+              ),
+            ],
           ),
         ),
       ),
--- frontend/lib/home/widgets/ToRegisterCard.dart
+++ frontend/lib/home/widgets/ToRegisterCard.dart
@@ -16,69 +16,65 @@ class ToRegisterCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(12.0),
         ),
         color: Color(0xFFFCDABC),
-        child: Center(
-          child: Container(
-            width: ScreenUtil.w(83),
-            height: ScreenUtil.h(16),
-
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  child: Text(
-                    '카드를 등록해주세요',
-                    style: AppFonts.suit(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w800,
-                        color: AppColors.textBlack),
-                  ),
-                ),
-
-                Text(
-                  '카드를 등록하시면 더 많은 정보를 알 수 있어요',
+        child: Container(
+          margin: EdgeInsets.only(left: 10, right: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Text(
+                  '카드를 등록해주세요',
                   style: AppFonts.suit(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w700,
-                      color: Color(0xFFCA9059)),
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
+                      color: AppColors.textBlack),
                 ),
-                SizedBox(height: ScreenUtil.h(2),),
-                Container(
-                  width: ScreenUtil.w(53),
-                  height: ScreenUtil.h(4),
-                  margin: EdgeInsets.only(left: 10),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => CardRegistration()));
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColors.mainWhite,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '카드 등록하러 가기', // 텍스트
-                          style: AppFonts.suit(
-                            color: AppColors.mainGreen,
-                            fontSize: 12,
-                            fontWeight: FontWeight.w800,
-                          ), // 텍스트 색상
-                        ),
-                        Icon(
-                          Icons.keyboard_arrow_right_rounded, // 아이콘
-                          color: AppColors.mainBlue, // 아이콘 색상
-                        ),
-                      ],
+              ),
+
+              Text(
+                '카드를 등록하시면 더 많은 정보를 알 수 있어요',
+                style: AppFonts.suit(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xFFCA9059)),
+              ),
+              SizedBox(height: ScreenUtil.h(2),),
+              Container(
+                width: ScreenUtil.w(53),
+                height: ScreenUtil.h(4),
+                margin: EdgeInsets.only(left: 10),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => CardRegistration()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.mainWhite,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
                     ),
                   ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '카드 등록하러 가기', // 텍스트
+                        style: AppFonts.suit(
+                          color: AppColors.mainGreen,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w800,
+                        ), // 텍스트 색상
+                      ),
+                      Icon(
+                        Icons.keyboard_arrow_right_rounded, // 아이콘
+                        color: AppColors.mainBlue, // 아이콘 색상
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
