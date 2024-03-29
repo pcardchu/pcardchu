299317a6a563a77603b31a3f1ffc7d6120ba9ed4
금예인 <tbc07061@naver.com>
2024-03-29 16:14:33 +0900

    🛠 fix:  FirstTokenRes.java 수정

--- backend/src/main/java/com/ssafy/pickachu/domain/user/response/FirstTokenRes.java
+++ backend/src/main/java/com/ssafy/pickachu/domain/user/response/FirstTokenRes.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
-@Schema(description = "2차 토큰 발급에 대한 응답 DTO")
+@Schema(description = "1차 토큰 발급에 대한 응답 DTO")
 @Builder
 @Data
 public class FirstTokenRes {
