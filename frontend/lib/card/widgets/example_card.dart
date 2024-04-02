c95621c4d4cb2306f57731979721646604a7aae3
박설연 <s303y@naver.com>
2024-04-03 04:14:03 +0900

    💠 feat: 카드화면 등록된 카드 보여주기 완료

--- frontend/lib/card/widgets/example_card.dart
+++ frontend/lib/card/widgets/example_card.dart
@@ -1,13 +1,12 @@
 import 'package:frontend/card/models/example_candidate_model.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/home/models/card_info_model.dart';
 
 class ExampleCard extends StatelessWidget {
-  final ExampleCandidateModel candidate;
+  //final ExampleCandidateModel candidate;
+  final CardInfoModel model;
 
-  const ExampleCard(
-      this.candidate, {
-        super.key,
-      });
+  const ExampleCard({super.key, required this.model});
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +15,11 @@ class ExampleCard extends StatelessWidget {
         height: 400,
         clipBehavior: Clip.hardEdge,
         decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(10)),
-    color: Colors.white
-    ),
-    child: Image.asset('assets/images/excard.png', fit: BoxFit.fill,)
-    );
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            color: Colors.white),
+        child: Image.asset(
+          model.cardImage ?? 'assets/images/xox_logo.png', // 기본 이미지 경로를 추가하세요.
+          fit: BoxFit.fill,
+        ));
   }
 }
--- frontend/lib/card/widgets/my_register_card.dart
+++ frontend/lib/card/widgets/my_register_card.dart
@@ -1,11 +1,15 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:frontend/providers/card_list_provider.dart';
+import 'package:frontend/home/models/card_info_model.dart';
+import 'package:frontend/card/widgets/example_card.dart';
 import 'package:frontend/utils/screen_util.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
-import 'package:frontend/card/models/example_candidate_model.dart';
-import 'package:frontend/card/widgets/example_card.dart';
 
 class MyRegisterCard extends StatefulWidget {
-  const MyRegisterCard({super.key});
+  const MyRegisterCard({Key? key}) : super(key: key);
 
   @override
   State<MyRegisterCard> createState() => _MyRegisterCardState();
@@ -14,23 +18,33 @@ class MyRegisterCard extends StatefulWidget {
 class _MyRegisterCardState extends State<MyRegisterCard> {
   final CardSwiperController controller = CardSwiperController();
 
-  final cards = candidates.map(ExampleCard.new).toList();
-
   @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
+  void initState() {
+    // TODO: implement initState
+    super.initState();
 
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // CardListProvider를 가져와서 getUserCardsList 메소드를 호출합니다.
+      final cardListProvider = Provider.of<CardListProvider>(context, listen: false);
+      cardListProvider.getUserCardsList();
+    });
+
+  }
   @override
   Widget build(BuildContext context) {
+    final cardListProvider = Provider.of<CardListProvider>(context);
+    final cards = cardListProvider.cardList
+        .map((cardInfo) => ExampleCard(model: cardInfo))
+        .toList();
+
+
+    final int numberOfCardsToShow = min(cards.length, 3);
+
     return Container(
       height: ScreenUtil.h(56),
-      //color: Colors.red,
-      padding: EdgeInsets.symmetric(
-        vertical: 40,
-      ),
-      child: Column(
+      padding: EdgeInsets.symmetric(vertical: 40),
+      child: cards.isNotEmpty
+          ? Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Flexible(
@@ -39,43 +53,26 @@ class _MyRegisterCardState extends State<MyRegisterCard> {
               cardsCount: cards.length,
               onSwipe: _onSwipe,
               onUndo: _onUndo,
-              numberOfCardsDisplayed: 2,
+              numberOfCardsDisplayed: numberOfCardsToShow,
               allowedSwipeDirection: AllowedSwipeDirection.only(down: true),
               backCardOffset: const Offset(0, -60),
               padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-              cardBuilder: (
-                context,
-                index,
-                horizontalThresholdPercentage,
-                verticalThresholdPercentage,
-              ) =>
-                  cards[index],
+              cardBuilder: (context, index, horizontalThresholdPercentage, verticalThresholdPercentage) => cards[index],
             ),
           ),
         ],
-      ),
+      )
+          : Center(child: CircularProgressIndicator()), // 또는 "등록된 카드가 없습니다" 텍스트 표시
     );
   }
-}
 
-bool _onSwipe(
-  int previousIndex,
-  int? currentIndex,
-  CardSwiperDirection direction,
-) {
-  debugPrint(
-    'The card $previousIndex was swiped to the ${direction.name}. Now the card $currentIndex is on top',
-  );
-  return true;
-}
+  bool _onSwipe(int previousIndex, int? currentIndex, CardSwiperDirection direction) {
+    debugPrint('The card $previousIndex was swiped to the ${direction.name}. Now the card $currentIndex is on top');
+    return true;
+  }
 
-bool _onUndo(
-  int? previousIndex,
-  int currentIndex,
-  CardSwiperDirection direction,
-) {
-  debugPrint(
-    'The card $currentIndex was undod from the ${direction.name}',
-  );
-  return true;
+  bool _onUndo(int? previousIndex, int currentIndex, CardSwiperDirection direction) {
+    debugPrint('The card $currentIndex was undod from the ${direction.name}');
+    return true;
+  }
 }
