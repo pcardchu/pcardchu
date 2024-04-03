288f49f9c409c2fd752430e4208b6650b50f89a6
박설연 <s303y@naver.com>
2024-04-03 15:19:11 +0900

    🛠 fix : 기본 이미지 대응

--- frontend/lib/card/widgets/example_card.dart
+++ frontend/lib/card/widgets/example_card.dart
@@ -23,12 +23,19 @@ class ExampleCard extends StatelessWidget {
               fit: BoxFit.fill,
               errorBuilder: (context, error, stackTrace) {
                 // 로딩 실패 시 기본 이미지 표시
-                return Image.asset('assets/images/xox_logo.png',
-                    fit: BoxFit.fill);
+                return Image.asset('assets/images/xox_logo.png', width: 36, height: 36,
+                    fit: BoxFit.cover);
               },
             )
-          : Image.asset('assets/images/xox_logo.png',
-              fit: BoxFit.fill), // 기본 이미지
+          : Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: const [Color(0xFFFF3868), Color(0xFFFFB49A)],
+          ),
+        ),
+      ), // 기본 이미지
     );
   }
 }
