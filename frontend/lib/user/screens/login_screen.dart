b911cc299dc9e4bf35a8726bc186316f73e744ac
박설연 <s303y@naver.com>
2024-04-05 15:28:25 +0900

    🛠 fix : home 버튼 삭제

--- frontend/lib/user/screens/login_screen.dart
+++ frontend/lib/user/screens/login_screen.dart
@@ -20,42 +20,42 @@ class LoginScreen extends StatelessWidget {
           children: [
             Image.asset('assets/images/app_icon.png', width: ScreenUtil.w(70),),
             SizedBox(height: ScreenUtil.h(20),),
-            ElevatedButton(
-              onPressed: () async {
-                //accessToken 강제로 가져오기
-                final Dio dio = Dio();
-                String url = "${dotenv.env['API_URL']}/auth-test/second";
-
-                var header = {
-                  'accept' : 'application/json',
-                };
-
-                try {
-                  final Response response = await dio.get(
-                    url,
-                    options: Options(headers: header),
-                  );
-
-                  print("accessToken 강제 발급 : ${response.data}");
-                  LoginProvider provider = Provider.of<LoginProvider>(context, listen: false);
-                  provider.secondJwt = JwtToken(isFirst: false, refreshToken: null, accessToken: response.data['accessToken']);
-
-                  //api 호출 테스트
-                  // Response<dynamic> test = await DioUtil().dio.get('/user/info');
-                  // print(test.data);
-
-                } catch(e) {
-                  print("accessToken 강제 발급 실패 : $e");
-                }
-
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) => const BottomNavScreen(),
-                  ),
-                );
-              },
-              child: const Text('HOME'),
-            ),
+            // ElevatedButton(
+            //   onPressed: () async {
+            //     //accessToken 강제로 가져오기
+            //     final Dio dio = Dio();
+            //     String url = "${dotenv.env['API_URL']}/auth-test/second";
+            //
+            //     var header = {
+            //       'accept' : 'application/json',
+            //     };
+            //
+            //     try {
+            //       final Response response = await dio.get(
+            //         url,
+            //         options: Options(headers: header),
+            //       );
+            //
+            //       print("accessToken 강제 발급 : ${response.data}");
+            //       LoginProvider provider = Provider.of<LoginProvider>(context, listen: false);
+            //       provider.secondJwt = JwtToken(isFirst: false, refreshToken: null, accessToken: response.data['accessToken']);
+            //
+            //       //api 호출 테스트
+            //       // Response<dynamic> test = await DioUtil().dio.get('/user/info');
+            //       // print(test.data);
+            //
+            //     } catch(e) {
+            //       print("accessToken 강제 발급 실패 : $e");
+            //     }
+            //
+            //     Navigator.of(context).push(
+            //       MaterialPageRoute(
+            //         builder: (_) => const BottomNavScreen(),
+            //       ),
+            //     );
+            //   },
+            //   child: const Text('HOME'),
+            // ),
             SizedBox(
               width: ScreenUtil.w(85),
               child: KakaoLoginButton(),
